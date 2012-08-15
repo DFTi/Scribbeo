02bf186c09b3369d4c5acb6759ad578a3b6377e2
Jonathan Silverman <jsilver@mdx.la>
2012-08-14 18:45:42 -0700

    0 warnings. no issues in build

--- Classes/DrawView.m
+++ Classes/DrawView.m
@@ -133,7 +133,7 @@
         return;
         
     if ([vc keyboardShows])  {                                      // 3
-        [[[[kAppDel viewController] newNote] autorelease] resignFirstResponder];
+        [[[[kAppDel viewController] theNewNote] autorelease] resignFirstResponder];
         return;
     }
     else if ((player || still) && [vc fullScreenMode]) {            // 4
